8ababd3373bd99eb0efb63b6ef4cc17e5cc87744
Melanie Menge <melanie.menge@protonmail.com>
2026-03-04 07:11:02 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import skflogo from './assets/SKF-logo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className='flex items-center max-w-sm'>
+          <img className="object-center"src={skflogo} alt="SKF logo" />
+        </div>
+        <h1 className="text-3xl passion-one-bold mb-4">Skate Kollektiv Furttal</h1>
+        <div className="card">
+          <h2>Coming soon...</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App

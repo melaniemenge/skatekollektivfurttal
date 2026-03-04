c073f6862e42563edd727c9900fb230c8efd09a2
Melanie Menge <melanie.menge@protonmail.com>
2026-03-04 08:11:18 +0100

    add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className='flex items-center max-w-sm'>
-          <img className="object-center"src={skflogo} alt="SKF logo" />
-        </div>
-        <h1 className="text-3xl passion-one-bold mb-4">Skate Kollektiv Furttal</h1>
-        <div className="card">
-          <h2>Coming soon...</h2>
-        </div>
+      <div className="flex flex-col items-center justify-center min-h-screen px-4">
+        <img
+          src={skflogo}
+          alt="SKF logo"
+          className="mb-6 w-32 h-32 object-contain sm:w-40 sm:h-40"
+        />
+        <h1 className="text-3xl sm:text-4xl font-bold mb-2 text-center">Skate Kollektiv Furttal</h1>
+        <h2 className="text-lg sm:text-xl text-center">Coming soon...</h2>
       </div>
     </>
   )

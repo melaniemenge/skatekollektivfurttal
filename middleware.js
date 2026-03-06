7fbc6016e9f89160a66cc21b9dc4fb9385cbb972
Melanie Menge <melanie.menge@protonmail.com>
2026-03-06 07:51:10 +0100

    remove config

--- middleware.js
+++
@@ -1,8 +0,0 @@
-// middleware.js
-export function setSecurityHeaders(req, res, next) {
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-  res.setHeader('Content-Security-Policy', "default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline';");
-  res.removeHeader && res.removeHeader('Server');
-  next();
-}

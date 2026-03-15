cddcda3fc5c9ef5628075554fdd89809f304c824
Melanie Menge <106156672+melaniemenge@users.noreply.github.com>
2026-03-15 22:04:23 +0100

    Remove CardFooter import from ProjekteEvents

--- src/ProjekteEvents.tsx
+++ src/ProjekteEvents.tsx
@@ -1,7 +1,7 @@
 
 import Footer from './components/Footer';
 import Header from './components/Header';
-import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
+import { Card, CardHeader, CardBody } from "@heroui/card";
 
 const ProjekteEvents = () => {
 

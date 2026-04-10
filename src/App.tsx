489b61ba81549965fb25470b58cd171c4d35d1a2
Melanie Menge <melanie.menge@protonmail.com>
2026-04-10 10:43:07 +0200

    correct issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import { Analytics } from "@vercel/analytics/react"
 import { Helmet } from 'react-helmet-async';
-import qrcode from './assets/qr-code.png';
 
 function App() {
   const navigate = useNavigate();
--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 
+const BASE_URL = 'https://www.skatekollektivfurttal.ch';
 const ContactForm = () => {
   const [form, setForm] = useState({
     name: '',
@@ -39,6 +40,7 @@ const ContactForm = () => {
   return (
     <>
       <Header />
+      <link rel="canonical" href={`${BASE_URL}${location.pathname}`} />
       <div className="flex flex-col items-center justify-center min-h-screen pt-12">
         <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-center">Kontakt</h1>
         <p className="max-w-lg text-center text-gray-700 mb-8">
